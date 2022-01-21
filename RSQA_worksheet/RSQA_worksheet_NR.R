bc4094c3e9ec1cfde51d4bf5e9faaf955cb757c0
Noe <nrnoereyna@gmail.com>
2022-01-21 12:15:03 -0600

    .

--- RSQA_worksheet/RSQA_worksheet_NR.R
+++ RSQA_worksheet/RSQA_worksheet_NR.R
@@ -11,9 +11,18 @@ View(mydata)
 exp_count <- mydata %>% 
   group_by(PARM_NM) %>% 
   summarise(count=n())
+
 # 12. create hist of exp_count showing frequency of 'counts'
 # this will allow use to see the frequency of datapoints of each paramater measurement
-ggplot(exp_count, aes(count)) +
+uinque_measurements_hist <- ggplot(exp_count, aes(count)) +
   geom_histogram(binwidth=100) +
-  labs(x='measurements done on a unique parameter')+
+  labs(title='Frequency [y] of total data points by unique parameters [x]',
+    x='measurements done on a unique parameter') +
   theme_classic()
+
+# how many counted once; other notation: count==1?
+exp_count_counted_once <- length(exp_count$PARM_NM[exp_count$count==1])
+
+# observe exp_count$count 
+exp_count <- exp_count[order(exp_count$count, decreasing=TRUE), ]
+head(exp_count)
